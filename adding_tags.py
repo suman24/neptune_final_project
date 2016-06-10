c4c3b18930dc1e59ede825ba8afb496fe2740248
suman <suman.kumar@uib.no>
2016-06-10 11:53:16 +0200

    update

--- adding_tags.py
+++ adding_tags.py
@@ -11,6 +11,7 @@ OutFileName = "reads100.txt"
 OutFile = open(OutFileName, 'w' )
 
 import random
+#using random functionality
 
 for Line in InFile:
 
@@ -22,7 +23,8 @@ for Line in InFile:
 
 	if Line [ 0 ] in nucl:
 		print (random.choice(seq1)) + Line
-
+#Line [0] in nucl looks for A/T/G/C in the first character of a line.
+#Line [-1] looks at the last character of a line
 
 	else:
 		print Line
@@ -32,7 +34,7 @@ for Line in InFile:
 
 InFile.close()
 
-
+#when running the program use '>' plus the outfile file.txt/fa to get an output file.
 
 
 
